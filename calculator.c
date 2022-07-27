31fef76cf4e25c63cd34bdacd0e2286b68923bda
AdiCoder03 <adityapoddar0903@gmail.com>
2022-07-27 16:01:42 +0530

    Added main function to calculator

---
+++ calculator.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+  int ch = 1;
+  while(ch > 0 && ch < 7)
+  {
+    printf("1. Addition\n2. Subtraction\n3. Multiplication\n4. Quotient of integer division\n5. Remainder of integer division\n6. Floating point division");
+    printf("\nEnter any other number to exit.\nEnter your choice: ");
+    scanf("%d", &ch);
+    switch(ch)
+    {
+      case 1: Add();
+      break;
+      case 2: Subtract();
+      break;
+      case 3: Multiplication();
+      break;
+      case 4: Quotient();
+      break;
+      case 5: Remainder();
+      break;
+      case 6: Divide();
+      break;
+    }
+  }
+  return 0;
+}
